d573f546a26fe7b84ffcd638eeb9dee62a3309b3
Yogeshpatgar44 <yogeshs.is21@sahyadri.edu.in>
2025-06-27 21:16:43 +0530

    Setup routing and basic pages

--- estimateai-frontend/src/App.jsx
+++ estimateai-frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-// import Estimate from './pages/Estimate';
-// import Login from './pages/Login';
-// import Register from './pages/Register';
+import Estimate from './pages/Estimate';
+import Login from './pages/Login';
+import Register from './pages/Register';
 import Navbar from './components/Navbar';
+import "./App.css"
 
 function App() {
   return (
