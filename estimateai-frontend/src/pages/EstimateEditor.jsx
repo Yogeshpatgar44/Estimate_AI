ce9a7e5f0e4381c0ac3f5dcfdc9a5911f0c7e4be
Yogeshpatgar44 <yogeshs.is21@sahyadri.edu.in>
2025-06-29 15:36:34 +0530

    updated etimate code

--- estimateai-frontend/src/pages/EstimateEditor.jsx
+++ estimateai-frontend/src/pages/EstimateEditor.jsx
@@ -1,53 +1,3 @@
-// // EstimateEditor.jsx
-// import React, { useState } from 'react';
-// import { Box, Tabs, Tab, Typography, Divider } from '@mui/material';
-// import EditEstimate from '../components/EditEstimate';
-// import PDFCustomization from '../components/PDFCustomization';
-// import PDFPreview from '../components/PDFPreview';
-// import ExportShare from '../components/ExportShare';
-// import Navbar from '../components/Navbar'
-
-
-// const EstimateEditor = ({ estimateData }) => {
-//   const [tab, setTab] = useState(0);
-
-//   const handleChange = (event, newValue) => setTab(newValue);
-
-//   return (
-//     <>
-//     <Navbar/>
-  
-//     <Box p={4}>
-      
-//     <Tabs
-//         value={tab}
-//         onChange={handleChange}
-//         centered
-//         sx={{ mb: 3 }}
-//       >
-//         <Tab label="Edit Estimate" sx={{ color: 'white',fontWeight:'bold' }} />
-//         <Tab label="PDF Customization" sx={{ color: 'white',fontWeight:'bold' }} />
-//         <Tab label="Preview PDF" sx={{ color: 'white',fontWeight:'bold' }} />
-//         <Tab label="Export & Share" sx={{ color: 'white',fontWeight:'bold' }} />
-//       </Tabs>
-
-
-//       <Divider sx={{ mb: 3 }} />
-
-//       {tab === 0 && <EditEstimate estimate={estimateData} />}
-//       {tab === 1 && <PDFCustomization />}
-//       {tab === 2 && <PDFPreview estimate={estimateData} />}
-//       {tab === 3 && <ExportShare estimate={estimateData} />}
-//     </Box>
-//     </>
-//   );
-// };
-
-// export default EstimateEditor;
-
-// EstimateEditor.jsx
-
-
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import {
@@ -104,7 +54,7 @@ const EstimateEditor = () => {
           <Tab label="Export & Share" sx={{ color: 'white', fontWeight: 'bold' }} />
         </Tabs>
 
-        <Divider sx={{ mb: 3, borderColor: 'white' }} />
+        {/* <Divider sx={{ mb: 3, borderColor: 'white' }} /> */}
 
         {loading ? (
           <Box textAlign="center" mt={5}>
