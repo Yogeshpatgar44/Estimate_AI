b9c344604195459ee4284b2227799d501974116c
Yogeshpatgar44 <yogeshs.is21@sahyadri.edu.in>
2025-06-30 03:07:21 +0530

    updated route

--- estimateai-backend/controllers/estimateController.js
+++ estimateai-backend/controllers/estimateController.js
@@ -156,3 +156,17 @@ exports.getEstimateById = async (req, res) => {
     res.status(500).json({ error: 'Server error' });
   }
 };
+
+exports.deleteEstimate = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deleted = await Estimate.findByIdAndDelete(id);
+    if (!deleted) {
+      return res.status(404).json({ message: 'Estimate not found' });
+    }
+    res.status(200).json({ message: 'Estimate deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ message: 'Server error', error });
+  }
+};
+
--- estimateai-backend/routes/estimateRoutes.js
+++ estimateai-backend/routes/estimateRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { generateEstimate,saveEstimate, getEstimates,updateEstimate,getEstimateById } = require('../controllers/estimateController');
+const { generateEstimate,saveEstimate, getEstimates,updateEstimate,getEstimateById,deleteEstimate } = require('../controllers/estimateController');
 const verifyToken = require('../middleware/authMiddleware');
 const requireAuth = require('../middleware/authMiddleware');
 
@@ -9,6 +9,9 @@ router.post('/save', verifyToken, saveEstimate);
 router.get('/history', verifyToken, getEstimates);
 router.patch('/:id', requireAuth, updateEstimate);
 router.get('/:id', requireAuth, getEstimateById);
+router.delete('/:id', requireAuth, deleteEstimate);
+
+
 
 
 module.exports = router;
