ac9a8a3c0ac48c87c86bd6dfdff0ace73e430eb3
Yogeshpatgar44 <yogeshs.is21@sahyadri.edu.in>
2025-06-30 20:48:14 +0530

    updated controller

--- estimateai-backend/server.js
+++ estimateai-backend/server.js
@@ -14,8 +14,6 @@ app.use(express.json({ limit: '10mb' }));
 // Routes
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/estimates', require('./routes/estimateRoutes'));
-app.use('/api/subscription', require('./routes/subscriptionRoutes'));
-
 
 // DB Connection
 connectDB();
