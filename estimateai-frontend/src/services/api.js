98b6dfb9439733d941f9240a499aa887e3c347a3
Yogeshpatgar44 <yogeshs.is21@sahyadri.edu.in>
2025-06-30 22:36:34 +0530

    updated Base url

--- estimateai-frontend/src/services/api.js
+++ estimateai-frontend/src/services/api.js
@@ -1,3 +1,3 @@
-const BASE_URL = "https://estimate-ai.onrender.com"; // Change to your backend URL if hosted
+const BASE_URL = "https://estimate-ai.onrender.com/api"; // Change to your backend URL if hosted
 
 export default BASE_URL;
